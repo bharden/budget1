bac3a74ee9adf32d19f503d49ff4362722cc6730
benny <ben.harden@sgiam.com>
2020-04-24 13:02:33 -0600

    Added server port 3005

--- back-end/server.js
+++ back-end/server.js
@@ -92,4 +92,4 @@ app.get("/api/clients/:id/jobs", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3005, () => console.log('Server listening on port 3005!'));
